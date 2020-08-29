c8bb89fba6e8dcf92acc4c045f2b20378f4f58b4
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-08-29 17:35:56 +0700

    :notebook: Added content for page

--- carousel.js
+++ carousel.js
@@ -1,6 +1,12 @@
 let index = 0;
 let minIndex = 0;
-let maxIndex = 4;
+let maxIndex = 5;
+
+function initializeCarousel() {
+  for (let i = minIndex + 1; i <= maxIndex; i++) {
+    document.getElementById(`card-content-data-${i}`).style.display = "none";
+  }
+}
 
 function nextPage() {
   if (index < maxIndex) {
@@ -20,4 +26,41 @@ function prevPage() {
     document.getElementById(`card-content-data-${index}`).style.display =
       "block";
   }
+}
+
+function generateSkillChip(arr) {
+  let container = document.getElementById("skill-container-content");
+  let content = "";
+  for (let i = 0; i < arr.length; i++) {
+    content += `<div class="skill-chip">${arr[i]}</div>`;
+  }
+
+  container.innerHTML = content;
+}
+
+function generateWorkListItem(arr) {
+  let container = document.getElementById("work-container-content");
+  let content = `
+  <div class="work-list-item">
+    <div style="font-weight: 600">
+      ${arr[0].name} (${arr[0].start} - ${arr[0].end})
+    </div>
+    <div>${arr[0].job}</div>
+  </div>
+  `;
+
+  for (let i = 1; i < arr.length; i++) {
+    content += `
+    <hr />
+    <div class="work-list-item">
+      <div style="font-weight: 600">
+        ${arr[i].name} (${arr[i].start} - ${arr[i].end})
+      </div>
+      <div>${arr[i].job}</div>
+    </div>
+    
+    `;
+  }
+
+  container.innerHTML = content;
 }
